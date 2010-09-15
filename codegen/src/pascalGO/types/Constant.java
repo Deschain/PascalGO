46a6c7fab35b815d681ba2e309454ed967d1cb0b
GelidElf <GelidElf@76835b46-2c6c-11de-94d6-134fa40061eb>
2010-09-15 00:05:49 +0000

    Added support for Type in constant, easier checks. Completed the test text satisfactorially.
    TODO:
    Comments, comments, comments
    GUI?
    Translate error messages to english, some of them
    check with heavier code.
    Remove informative sysos (variable integer created, etc etc)

--- codegen/src/pascalGO/types/Constant.java
+++ codegen/src/pascalGO/types/Constant.java
@@ -3,10 +3,18 @@ package pascalGO.types;
 public class Constant extends BasicType {
 
 	private String value;
+	private String type;
 	
 	public Constant(String name, String value){
 		super(name);
 		this.value = value;
+		this.type = null;
+	}
+	
+	public Constant(String name, String value,String type){
+		super(name);
+		this.value = value;
+		this.type = type;
 	}
 
 	public String getValue() {
@@ -17,6 +25,14 @@ public class Constant extends BasicType {
 		this.value = value;
 	}
 
+	public String getType(){
+		return type;
+	}
+	
+	public void setType(String type){
+		this.type = type;
+	}
+	
 	public boolean isNumeric(){
 		try{
 			Integer.parseInt(value);
